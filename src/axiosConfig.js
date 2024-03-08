6f2fb8047fbe70e3ab2bb989ebc4ad704b5dc378
seli1990 <153053446+seli1990@users.noreply.github.com>
2024-03-08 17:24:31 -0600

    endpoint changed

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosBase = axios.create({
 
-    baseURL:'http://localhost:5500/api'
+    baseURL:'https://evangadi-forum-backend-awan.onrender.com/api'
 })
 
 export default axiosBase
